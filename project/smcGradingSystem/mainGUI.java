04536b54977b648bab2781ec2ecf8d717af9b0b6
juneGithub <75412999+rrokusan@users.noreply.github.com>
2024-01-10 20:54:53 +0800

    update

--- project/smcGradingSystem/mainGUI.java
+++ project/smcGradingSystem/mainGUI.java
@@ -8,6 +8,11 @@ public class mainGUI {
     JFrame frame = new JFrame();
 
     
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+    frame.setResizable(false);
+    frame.setSize(1920, 1080);
+		frame.setVisible(true);
   }
   public static void main(String args[]){
     SwingUtilities.invokeLater(new Runnable(){
